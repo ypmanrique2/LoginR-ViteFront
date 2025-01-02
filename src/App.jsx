797d11045cbcf5002db918460d4a755a71568c8f
Yadin Paulo Manrique Marquez <ypmanrique2@misena.edu.co>
2025-01-01 19:34:57 -0500

    actualizar ruta back-end en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,20 @@ import Registro from './Registro'
 function App() {
   const [usuario, setUsuario] = useState('')
   const [clave, setClave] = useState('')
+  const [usuarioRegistro, setUsuarioRegistro] = useState('')
+  const [claveRegistro, setClaveRegistro] = useState('')
   const [logueado, setLogueado] = useState(false)
-  const [recargar, setRecargar] = useState(false)
+  const [usuarios, setUsuarios] = useState([])
+  const [rol, setRol] = useState('')
+  //const [recargar, setRecargar] = useState(false)
+
+  function cambiarUsuarioRegistro(evento) {
+    setUsuarioRegistro(evento.target.value)
+  }
+
+  function cambiarClaveRegistro(evento) {
+    setClaveRegistro(evento.target.value)
+  }
 
   function cambiarUsuario(evento) {
     setUsuario(evento.target.value)
@@ -18,23 +30,49 @@ function App() {
     setClave(evento.target.value)
   }
 
-  function recargarAhora() {
+  /* function recargarAhora() {
     setRecargar(!recargar)
-  }
+  } */
 
   async function ingresar() {
-    const peticion = await fetch('http://localhost:3000/login?usuario=' + usuario + '&clave=' + clave, { credentials: 'include' })
+    const peticion = await fetch('https://loginexpress-1-8pdh.onrender.com/login?usuario=' + usuario + '&clave=' + clave, { credentials: 'include' })
     if (peticion.ok) {
+      const datos = await peticion.json();
+      if (datos.rol == 'ADMINISTRADOR') {
+        setRol('ADMINISTRADOR')
+      } else {
+        setRol('USUARIO')
+      }
       setLogueado(true)
+      obtenerUsuarios()
     } else {
       alert('Usuario o clave incorrectos')
     }
   }
 
+  async function registro() {
+    const peticion = await fetch('https://loginexpress-1-8pdh.onrender.com/registrar?usuario=' + usuarioRegistro + '&clave=' + claveRegistro)
+    if (peticion.ok) {
+      alert('Usuario registrado')
+      setLogueado(true)
+      obtenerUsuarios()
+    } else {
+      alert('No se pudo registrar el usuario')
+    }
+  }
+
   async function validar() {
-    const peticion = await fetch('http://localhost:3000/validar', { credentials: 'include' })
+    const peticion = await fetch('https://loginexpress-1-8pdh.onrender.com/validar', { credentials: 'include' })
     if (peticion.ok) {
       setLogueado(true)
+      obtenerUsuarios()
+    }
+  }
+
+  async function obtenerUsuarios() {
+    const peticion = await fetch('https://loginexpress-1-8pdh.onrender.com/usuarios', { credentials: 'include' })
+    if (peticion.ok) {
+      setUsuarios((await peticion.json()))
     }
   }
 
@@ -42,25 +80,33 @@ function App() {
     validar()
   }, [])
 
-  if (logueado) {
-    return (
-
-      <>
-        <Registro recargarAhora={recargarAhora} />
-        <Conversor />
-        <Usuarios recargar={recargar} />
-      </>)
+  async function eliminarUsuario(id) {
+    const peticion = await fetch('https://loginexpress-1-8pdh.onrender.com/usuarios?id=' + id, { credentials: 'include', method: 'DELETE' })
+    if (peticion.ok) {
+      alert('Usuario eliminado')
+    }
   }
-
-  return (
-    <>
-      <h1>Inicio de sesión</h1>
-      <input placeholder='Usuario' type="text" name="usuario" id="usuario" value={usuario} onChange={cambiarUsuario} />
-      <input placeholder='Clave' type="password" name="clave" id="clave" value={clave} onChange={cambiarClave} />
-      <button onClick={ingresar}>Ingresar</button>
-
-    </>
-  )
+  /*
+    if (logueado) {
+      return (
+  
+        <>
+          <Registro recargarAhora={recargarAhora} />
+          <Conversor />
+          <Usuarios recargar={recargar} />
+        </>)
+    }
+  
+    return (
+      <>
+        <h1>Inicio de sesión</h1>
+        <input placeholder='Usuario' type="text" name="usuario" id="usuario" value={usuario} onChange={cambiarUsuario} />
+        <input placeholder='Clave' type="password" name="clave" id="clave" value={clave} onChange={cambiarClave} />
+        <button onClick={ingresar}>Ingresar</button>
+  
+      </>
+    )
+  } */
 }
 
-export default App
+export default App;
