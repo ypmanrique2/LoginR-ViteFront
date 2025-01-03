1b34b39711e6bc4d8f5d8e0870dadf67774e3458
Yadin Paulo Manrique Marquez <ypmanrique2@misena.edu.co>
2025-01-03 08:57:47 -0500

    correccion en vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -7,5 +11,5 @@ export default defineConfig({
       '/usuarios': 'https://loginexpress-1-8pdh.onrender.com',
       '/registrar': 'https://loginexpress-1-8pdh.onrender.com',
     },
-  },
+  }
 });
