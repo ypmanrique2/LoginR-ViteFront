89814fc1d826d6c486c6514dca79894a0a2397c9
Yadin Paulo Manrique Marquez <ypmanrique2@misena.edu.co>
2025-02-22 13:06:13 -0500

    changed vite.config.js added plugins: [react()], and port 10000

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   server: {
-    port: process.env.PORT || 4173,  // Usar el puerto asignado por Render
+    port: process.env.PORT || 10000,  // Usar el puerto asignado por Render
     host: true  // Asegurarse de que se pueda acceder desde fuera
   }
 });
